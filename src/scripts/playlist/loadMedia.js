4134599ce6571f25e0d93ac7fa387db9cd1284f4
Bruno Cardoso <bc13092002@gmail.com>
2023-11-02 22:44:20 -0300

    Iniciando o projeto npm

--- src/scripts/playlist/loadMedia.js
+++
@@ -1,6 +0,0 @@
-export default async function loadMedia(path, title){
-    players.classList.toggle("load-audio", true)
-    localStorage.setItem("tempFunc", 1);
-    localStorage.setItem("tempTitle", title);
-    localStorage.setItem("tempPath", path);
-}
